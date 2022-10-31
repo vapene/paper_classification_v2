9f3cbc65c18e7085f318297e86b473b27e4730a9
vapene <84373236+vapene@users.noreply.github.com>
2022-10-31 11:26:08 +0900

    Update models.py

--- models.py
+++ models.py
@@ -3,31 +3,72 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.models import resnet50, alexnet, vgg11, densenet121
 
+
+
+class resnet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.resnet = resnet50(pretrained=True)
+        self.classifier = nn.Linear(1000,5)
+
+    def forward(self, x):
+        x = self.resnet(x)
+        x = self.classifier(x)
+
+        return x
+
+class alex(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.alexnet = alexnet(pretrained=True)
+        self.classifier = nn.Linear(1000,5)
+    def forward(self,x):
+        x= self.alexnet(x)
+        x= self.classifier(x)
+        return x
+
+class vgg(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.vgg11 = vgg11(pretrained=True)
+        self.classifier = nn.Linear(1000,5)
+    def forward(self,x):
+        x= self.vgg11(x)
+        x= self.classifier(x)
+        return x
+
+class dense(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.densenet = densenet121(pretrained=True)
+        self.classifier = nn.Linear(1000,5)
+    def forward(self,x):
+        x= self.densenet(x)
+        x= self.classifier(x)
+        return x
+
 class M3(nn.Module):
     def __init__(self):
         super(M3, self).__init__()
-        # in_channel, out_channel, kernel_size
-        self.conv1 = nn.Conv2d(3, 32, 3, bias=False)       # output becomes 26x26
+        self.conv1 = nn.Conv2d(3, 32, 3, bias=False)
         self.conv1_bn = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 48, 3, bias=False)      # output becomes 24x24
+        self.conv2 = nn.Conv2d(32, 48, 3, bias=False)
         self.conv2_bn = nn.BatchNorm2d(48)
-        self.conv3 = nn.Conv2d(48, 64, 3, bias=False)      # output becomes 22x22
+        self.conv3 = nn.Conv2d(48, 64, 3, bias=False)
         self.conv3_bn = nn.BatchNorm2d(64)
-        self.conv4 = nn.Conv2d(64, 80, 3, bias=False)      # output becomes 20x20
+        self.conv4 = nn.Conv2d(64, 80, 3, bias=False)
         self.conv4_bn = nn.BatchNorm2d(80)
-        self.conv5 = nn.Conv2d(80, 96, 3, bias=False)      # output becomes 18x18
-        self.conv5_bn = nn.BatchNorm2d(96)
-        self.conv6 = nn.Conv2d(96, 112, 3, bias=False)     # output becomes 16x16
-        self.conv6_bn = nn.BatchNorm2d(112)
-        self.conv7 = nn.Conv2d(112, 128, 3, bias=False)    # output becomes 14x14
-        self.conv7_bn = nn.BatchNorm2d(128)
-        self.conv8 = nn.Conv2d(128, 144, 3, bias=False)    # output becomes 12x12
-        self.conv8_bn = nn.BatchNorm2d(144)
-        self.conv9 = nn.Conv2d(144, 160, 3, bias=False)    # output becomes 10x10
-        self.conv9_bn = nn.BatchNorm2d(160)
-        self.conv10 = nn.Conv2d(160, 176, 3, bias=False)   # output becomes 8x8
-        self.conv10_bn = nn.BatchNorm2d(176)
-        self.fc1 = nn.Linear(720896, 5, bias=False)
+        self.conv5 = nn.Conv2d(80, 112, 3, bias=False)
+        self.conv5_bn = nn.BatchNorm2d(112)
+        self.conv6 = nn.Conv2d(112, 128, 3, bias=False)
+        self.conv6_bn = nn.BatchNorm2d(128)
+        self.conv7 = nn.Conv2d(128, 144, 3, bias=False)
+        self.conv7_bn = nn.BatchNorm2d(144)
+        self.conv8 = nn.Conv2d(144, 160, 3, bias=False)
+        self.conv8_bn = nn.BatchNorm2d(160)
+        self.conv9 = nn.Conv2d(160, 176, 3, bias=False)
+        self.conv9_bn = nn.BatchNorm2d(176)
+        self.fc1 = nn.Linear(766656, 5, bias=False)
         self.fc1_bn = nn.BatchNorm1d(5)
     def get_logits(self, x):
         x = (x - 0.5) * 2.0
@@ -40,15 +81,13 @@ class M3(nn.Module):
         conv7 = F.relu(self.conv7_bn(self.conv7(conv6)))
         conv8 = F.relu(self.conv8_bn(self.conv8(conv7)))
         conv9 = F.relu(self.conv9_bn(self.conv9(conv8)))
-        conv10 = F.relu(self.conv10_bn(self.conv10(conv9)))
-        flat1 = torch.flatten(conv10.permute(0, 2, 3, 1), 1)
+        # conv10 = F.relu(self.conv10_bn(self.conv10(conv9)))
+        flat1 = torch.flatten(conv9.permute(0, 2, 3, 1), 1)
         logits = self.fc1_bn(self.fc1(flat1))
         return logits
     def forward(self, x):
         logits = self.get_logits(x)
         return F.log_softmax(logits, dim=1)
-
-
 
 class M5(nn.Module):
     def __init__(self):
@@ -106,46 +145,3 @@ class M7(nn.Module):
     def forward(self, x):
         logits = self.get_logits(x)
         return F.log_softmax(logits, dim=1)
-
-
-class resnet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.resnet = resnet50(pretrained=True)
-        self.classifier = nn.Linear(1000,5)
-
-    def forward(self, x):
-        x = self.resnet(x)
-        x = self.classifier(x)
-
-        return x
-
-class alex(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.alexnet = alexnet(pretrained=True)
-        self.classifier = nn.Linear(1000,5)
-    def forward(self,x):
-        x= self.alexnet(x)
-        x= self.classifier(x)
-        return x
-
-class vgg(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.vgg11 = vgg11(pretrained=True)
-        self.classifier = nn.Linear(1000,5)
-    def forward(self,x):
-        x= self.vgg11(x)
-        x= self.classifier(x)
-        return x
-
-class dense(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.densenet = densenet121(pretrained=True)
-        self.classifier = nn.Linear(1000,5)
-    def forward(self,x):
-        x= self.densenet(x)
-        x= self.classifier(x)
-        return x
